21712e1bba9d34da273bc37c86e2932783eaecfc
Ryan Ovsienko <strikeanerve@gmail.com>
2013-12-22 12:52:17 -0600

    Updated access keys in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,9 @@ Pinteresting::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['hookahtime'],
+      :access_key_id => ENV['AKIAJNYDMTKJJDHV2BXA'],
+      :secret_access_key => ENV['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']
     }
   }
 end
